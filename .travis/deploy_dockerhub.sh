af62dcf4603855710c902ef38512ae680b01a7b7
Anssi <anssi@fns.fi>
2018-05-27 21:42:16 +0300

    travis_repo_slug fixed

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG
+# docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker build -f Dockerfile -t $DOCKER_PATH .
+
+# docker push $TRAVIS_REPO_SLUG
+docker push $DOCKER_PATH
