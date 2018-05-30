3a7a6136d3f49334db96ff09baf3688757327e84
Anssi <anssi@fns.fi>
2018-05-30 21:36:31 +0300

    added --tag param

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
+DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO
 #docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker login --username $DOCKER_USER --password $DOCKER_PASS
 
@@ -10,7 +10,8 @@ else
 fi
 # docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 #docker build Dockerfile -t $DOCKER_PATH .
-docker build Dockerfile $DOCKER_PATH .
+#docker build Dockerfile $DOCKER_PATH .
+docker build Dockerfile --tag $DOCKER_PATH:$TAG .
 
 # docker push $TRAVIS_REPO_SLUG
 docker push $DOCKER_PATH
