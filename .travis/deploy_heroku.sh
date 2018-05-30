b3c0d86ef2dfa29e4b34343cff7c92c7ac26a4bc
Anssi <anssi@fns.fi>
2018-05-30 20:04:41 +0300

    added curl and release cmds

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,10 +1,14 @@
+
+#wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install heroku-container-registry
+docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:push web --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
+
+# original
 #!/bin/sh
 # wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 # heroku plugins:install heroku-container-registry
 # docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
 # heroku container:push web --app $HEROKU_APP_NAME
-
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install heroku-container-registry
-docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
