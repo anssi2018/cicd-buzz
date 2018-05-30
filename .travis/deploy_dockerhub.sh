73b8e32c2f5bbb1e16bfdac4c86af111be598bc5
Anssi <anssi@fns.fi>
2018-05-30 22:21:40 +0300

    trying with travis_repo_slug

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO
+#DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO
+DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
 #docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker login --username $DOCKER_USER --password $DOCKER_PASS
 
@@ -8,10 +9,11 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-# docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-#docker build Dockerfile -t $DOCKER_PATH .
-#docker build Dockerfile $DOCKER_PATH .
-docker build Dockerfile -t $DOCKER_PATH:$TAG .
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+# -docker build Dockerfile -t $DOCKER_PATH .
 
-# docker push $TRAVIS_REPO_SLUG
-docker push $DOCKER_PATH
+#docker build Dockerfile $DOCKER_PATH .
+#docker build Dockerfile -t $DOCKER_PATH:$TAG .
+
+docker push $TRAVIS_REPO_SLUG
+# -docker push $DOCKER_PATH
