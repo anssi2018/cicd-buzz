95bb63395b5e1caf3d25c40a91cad70303d8d1f8
Anssi <anssi@fns.fi>
2018-05-30 22:40:38 +0300

    comments removed

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO
 DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
 #docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker login --username $DOCKER_USER --password $DOCKER_PASS
@@ -9,11 +8,8 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-# -docker build Dockerfile -t $DOCKER_PATH .
+#docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker build Dockerfile -t $DOCKER_PATH .
 
-#docker build Dockerfile $DOCKER_PATH .
-#docker build Dockerfile -t $DOCKER_PATH:$TAG .
-
-docker push $TRAVIS_REPO_SLUG
-# -docker push $DOCKER_PATH
+#docker push $TRAVIS_REPO_SLUG
+docker push $DOCKER_PATH
